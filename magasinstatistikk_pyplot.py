f1d92843f2bf0403f03ca5708ab6650136389809
MariaaAT <mat9296@gmail.com>
2023-01-02 20:47:03 +0100

    historisk data

--- magasinstatistikk_pyplot.py
+++ magasinstatistikk_pyplot.py
@@ -56,8 +56,6 @@ def filling_capacity(magasin, omrnr: int, year: str):
 
         fig.add_trace(go.Scatter(x=x1, y=y1, mode="lines", name="Filling Capacity in VASS Region"))
         fig.add_trace(go.Scatter(x=x1, y=y3, mode="lines", name="SMA in VASS Region"))
-        #newnames = {"fyllingsgrad": "Filling Capacity in VASS Region", "SMA5": "SMA in VASS Region", "x1": "Date"}
-        #fig.for_each_trace(lambda t: t.update(name=newnames[t.name], legendgroup=newnames[t.name]))
 
     fig.update_layout(legend=dict(title=None, orientation="h", y=0.9, yanchor="bottom", x=0.5, xanchor="center"),
                       title=dict(text=f"Lake Filling Capacity in {region_to_name(omrnr)} Norway since {year}", y=1),
@@ -67,6 +65,47 @@ def filling_capacity(magasin, omrnr: int, year: str):
     fig.update_xaxes(title=dict(text="Date"))
 
     # fig.show() -> This opens a new tab on Google
+    return fig
+
+def filled_plot(magasin, omrnr: int):
+    df = pd.DataFrame(magasin, index=range(len(magasin)))  # DF created
+    df['dato_Id'] = pd.to_datetime(df['dato_Id'])  # Convert dates into datetime dtype
+
+    territory = df[df["omrnr"] == omrnr]  # data from territory
+    territory_EL = territory[territory["omrType"] == "EL"]  # from each territory, EL region selected
+    territory_EL_week = territory_EL.sort_values("iso_uke")  # sorted by week
+
+    max_values = []
+    min_values = []
+    for n in range(1, 53):
+        week_n = territory_EL_week[territory_EL_week["iso_uke"] == n]
+        territory_EL_max = week_n['fyllingsgrad'].max()
+        territory_EL_min = week_n['fyllingsgrad'].min()
+        max_values.append(territory_EL_max)
+        min_values.append(territory_EL_min)
+
+    y_values = territory_EL.sort_values("dato_Id")
+    y_values = y_values.tail(53)
+    y_values = list(y_values["fyllingsgrad"])
+
+    # Second DF created
+    DF = pd.Series(max_values, name="max_values").to_frame()
+    DF["min_values"] = pd.Series(min_values)
+    DF["y_values"] = pd.Series(y_values)
+    DF["week"] = range(1, 53)
+    print(DF)
+
+    fig = go.Figure()
+
+    # plot max_values
+    fig.add_trace(go.Scatter(x=DF["week"], y=DF["max_values"], mode="lines", name="Historisk max",
+                             line=dict(color="lightskyblue")))  # tonexty down
+    # plot min_values
+    fig.add_trace(go.Scatter(x=DF["week"], y=DF["min_values"], mode="lines", name="Historisk min",
+                             line=dict(color="lightskyblue"), fill='tonextx', fillcolor="lightskyblue"))  # tonextx up
+    # plot y_values
+    fig.add_trace(go.Scatter(x=DF["week"], y=DF["y_values"], mode="lines", name=f"Filling Capacity",
+                             line=dict(color="black")))
     return fig
 
 def load_magasin(url):
@@ -82,6 +121,6 @@ st.write('You selected region: ', f"{omrnr} - {region_to_name(omrnr)} Norway")
 magasin = load_magasin("https://biapi.nve.no/magasinstatistikk/api/Magasinstatistikk/HentOffentligData")
 
 try:
-    st.write(filling_capacity(magasin, omrnr, "2015"))
+    st.write(filled_plot(magasin, omrnr))
 except:
     print(traceback.format_exc())
